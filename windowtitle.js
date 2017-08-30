ed4787226afde400ad8c130f52b8064c77f02bff
zeteref <zeteref@gmail.com>
2017-08-30 15:51:23 +0200

    script template added

---
+++ windowtitle.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         keepassytitle
+// @namespace    http://discworld
+// @version      0.1
+// @description  add domain to window title to work with keepass
+// @author       zeteref
+// @match      *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
