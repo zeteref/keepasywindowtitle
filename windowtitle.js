36e5f79ba09988d52c953ab7520578b540a1222a
zeteref <zeteref@gmail.com>
2018-05-08 14:29:10 +0200

    use mutation observer

--- windowtitle.js
+++ windowtitle.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         keepassytitle
 // @namespace    https://github.com/zeteref/keepasywindowtitle/
-// @version      0.3
+// @version      0.3.1
 // @description  add domain to window title to work with keepass
 // @author       zeteref
 // @match      *://*/*
@@ -9,7 +9,30 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-
-     document.title = document.title + " - [keepass:" + document.location + "]";
+      'use strict';
+      
+      function add_id(title) {
+              return title + ' |keepass:' + document.location + '|';
+            }
+      
+      document.title = add_id(document.title);
+      
+      new MutationObserver(function(mutations) {
+              
+              var new_title = mutations[0].addedNodes[0].textContent;
+              console.log(new_title);
+              if (new_title.search('keepass:') != -1) {
+                        console.log('returning');
+                        return
+                      }
+              
+              new_title = add_id(new_title);
+              document.title = new_title; 
+              
+            }).observe(
+                    document.querySelector('title'),
+                    { subtree: true, characterData: true, childList: true }
+                  );
+         
 })();
+
